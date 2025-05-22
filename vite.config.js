191db72c571b13c547ad4cc0407066578266e56d
patelshiv86484 <patelshiv86484@gmail.com>
2025-05-22 16:40:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
    server:{
     proxy:{
-      "/api":"http://localhost:8000",//now wherever axios.get() start from /api it is replaced by http://localhost:8000/api means in backend this url is passed so no need to whitelist as proxy is done because port:3000 is for backend so no CORS error.
+        "/api":"https://full-stack-blog-webapp-backend.onrender.com"
+      // "/api":"http://localhost:8000",//now wherever axios.get() start from /api it is replaced by http://localhost:8000/api means in backend this url is passed so no need to whitelist as proxy is done because port:3000 is for backend so no CORS error.
     }
   },
   plugins: [react()],
